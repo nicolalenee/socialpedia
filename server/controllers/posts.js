c33daa0ecf2e7028e811974d53cb292a8754ce0e
cola-mars <86696492+cola-mars@users.noreply.github.com>
2022-12-06 14:09:37 -0500

    create homepage widgets

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -15,7 +15,7 @@ export const createPost = async(req, res) => {
       userPicturePath: user.picturePath,
       picturePath,
       likes: {},
-      comments: {}
+      comments: []
     })
     // save the post
     await newPost.save();
@@ -31,8 +31,8 @@ export const createPost = async(req, res) => {
 /** READ */
 export const getFeedPosts = async(req, res) => {
   try {
-    const posts = await Post.find();
-    res.status(200).json(posts);
+    const post = await Post.find();
+    res.status(200).json(post);
   } catch (err) {
     res.status(404).json({ message: err.message })
   }
@@ -41,8 +41,8 @@ export const getFeedPosts = async(req, res) => {
 export const getUserPosts = async(req, res) => {
   try {
     const { userId } = req.params;
-    const posts = await Post.find({ userId });
-    res.status(200).json(posts);
+    const post = await Post.find({ userId });
+    res.status(200).json(post);
   } catch (err) {
     res.status(404).json({ message: err.message })
   }
